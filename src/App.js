99b3f8d0e2d16952e6b79c8c41351ee64d8daeba
rezahatampoor <rezahtms@gmail.com>
2024-02-03 02:42:17 +0330

    created accordion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>accordion</div>;
+  const faqs = [
+    {
+      title: "Where are these chairs assembled?",
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+    },
+    {
+      title: "How long do I have to return my chair?",
+      text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+    },
+    {
+      title: "Do you ship to countries outside the EU?",
+      text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+    },
+  ];
+
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
 }
+
+const Accordion = ({ data }) => {
+  return (
+    <div className="accordion">
+      {data.map((item, index) => (
+        <AccordionItem item={item} number={index + 1} key={index + 1} />
+      ))}
+    </div>
+  );
+};
+
+const AccordionItem = ({ item, number }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div
+      className={`item ${isOpen && "open"}`}
+      onClick={() => setIsOpen((current) => !current)}
+    >
+      <p className="number">
+        {number < 10 && 0}
+        {number}
+      </p>
+      <p className="title">{item.title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{item.text}</div>}
+    </div>
+  );
+};
 
 export default App;
